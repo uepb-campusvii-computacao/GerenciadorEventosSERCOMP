cdaf866574c4b414e2c5ed126b599e700ae42172
Nimsay01 <yasmingg123tt@gmail.com>
2024-05-02 23:31:18 -0300

    Documentação do Swagger

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,19 @@ import express from "express";
 import cors from "cors"
 import * as dotenv from 'dotenv';
 import routes from "./routes";
-
+import swaggerUi from "swagger-ui-express";
+import SwaggerDocs from "../src/swagger.json";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/doc-api", swaggerUi.serve, swaggerUi.setup(SwaggerDocs))
+
 const FRONTEND_URL = process.env.FRONTEND_URL || ""
 const GERENCIADOR_URL = process.env.GERENCIADOR_URL || ""
+
 
 app.use(cors({
     origin: [FRONTEND_URL, GERENCIADOR_URL],
@@ -26,3 +30,4 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => (
     console.log("Http server running! on port " + PORT)
 ));
+
