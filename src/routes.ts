4a602873af9f1de0e37a6c8a31353a432fec4aae
Nimsay01 <yasmingg123tt@gmail.com>
2024-05-17 13:04:59 -0300

    Colocando mais rotas

--- src/routes.ts
+++ src/routes.ts
@@ -44,13 +44,10 @@ import { checkToken } from "./lib/ensureAuthenticate";
 
 const routes = Router();
 
-// Rotas Públicas (Mercardo)
+// Rotas Públicas (Mercado)
 routes.post("/marketplace", createOrder);
 routes.get("/marketplace/user/:user_id", getOrders);
-routes.post(
-  "/marketplace/:pagamento_id/realizar-pagamento",
-  realizarPagamentoVenda
-);
+routes.post("/marketplace/:pagamento_id/realizar-pagamento",realizarPagamentoVenda);
 routes.get("/events/:event_id/produtos", getAllProductsInEvent);
 
 // Rotas Públicas
@@ -59,20 +56,10 @@ routes.get("/events/:event_id/lotes", getLotesInEvent);
 routes.post("/register/:event_id", registerParticipanteInEvent);
 routes.post("/events/:event_id/inscricoes/find", getLoteIdAndUserId);
 routes.get("/lote/:lote_id/inscricoes/user/:user_id/", getUserInformation);
-routes.post(
-  "/lote/:lote_id/user/:user_id/realizar-pagamento",
-  realizarPagamento
-);
-routes.get(
-  "/events/:id_evento/atividades",
-  getAllActivitiesInEventOrdenateByTipo
-);
+routes.post("/lote/:lote_id/user/:user_id/realizar-pagamento",realizarPagamento);
+routes.get("/events/:id_evento/atividades",getAllActivitiesInEventOrdenateByTipo);
 routes.get("/pagamento/user/:user_id/lote/:lote_id", getUserInscricao);
-
-routes.get(
-  "/admin/events/:id_evento/inscricoes/todos",
-  getAllFinancialInformationsInEvent
-);
+routes.get("/admin/events/:id_evento/inscricoes/todos",getAllFinancialInformationsInEvent);
 
 // Rotas para usuários (com autenticação)
 const userRoutes = Router();
@@ -87,33 +74,18 @@ const eventRoutes = Router();
 eventRoutes.use(checkToken);
 eventRoutes.get("/admin/events/:event_id/dashboard", getFinancialInformation);
 eventRoutes.get("/admin/user/:user_id/events", getAllEventsByIdUser);
-eventRoutes.get(
-  "/admin/events/:id_evento/inscricoes",
-  getAllSubscribersInEvent
-);
-eventRoutes.put(
-  "/admin/events/:event_id/inscricoes/credenciamento/:user_id",
-  changeEventCredenciamentoValue
-);
+eventRoutes.get("/admin/events/:id_evento/inscricoes",getAllSubscribersInEvent);
+eventRoutes.put("/admin/events/:event_id/inscricoes/credenciamento/:user_id",changeEventCredenciamentoValue);
 eventRoutes.get("/admin/events/:id_evento/atividades", getAllActivitiesInEvent);
 
 // Rotas para atividades (com autenticação)
 const activityRoutes = Router();
 activityRoutes.use(checkToken);
-activityRoutes.get(
-  "/admin/atividades/:id_atividade/inscricoes",
-  getSubscribersInActivity
-);
+activityRoutes.get("/admin/atividades/:id_atividade/inscricoes",getSubscribersInActivity);
 activityRoutes.get("/admin/atividades/:atividade_id", getActivityById);
 activityRoutes.put("/admin/atividades/:atividade_id", updateActivity);
-activityRoutes.put(
-  "/admin/atividades/:atividade_id/inscricoes/:user_id/frequencia",
-  changeActivityPresencaValue
-);
-activityRoutes.put(
-  "/admin/user/:user_id/atividades/troca",
-  upadateUserActivity
-);
+activityRoutes.put("/admin/atividades/:atividade_id/inscricoes/:user_id/frequencia",changeActivityPresencaValue);
+activityRoutes.put("/admin/user/:user_id/atividades/troca",upadateUserActivity);
 
 // Rotas para o mercado (com autenticação)
 const marketRoutes = Router();
